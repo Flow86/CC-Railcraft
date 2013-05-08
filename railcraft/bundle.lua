20820eb9775b70f86c39bf893270ba5e041c428f
Flow86 <github@fd.mytrap.de>
2013-05-08 15:34:34 +0200

    fix up save

--- railcraft/bundle.lua
+++ railcraft/bundle.lua
@@ -19,3 +19,40 @@ function pulseBundledState(cable, color, state)
 	sleep(0.1)
 	setBundledState(cable, color, not state)
 end
+
+function shallowcompare(a, b)
+    local a_type = type(a)
+    local b_type = type(b)
+	
+	if a_type ~= b_type then
+		return false
+	end
+	
+	if a_type == 'table' then
+		for a_key, a_value in pairs(a) do
+			if b[a_key] ~= a_value then
+				return false
+			end
+		end
+	else
+		if a ~= b then
+			return false
+		end
+	end
+	
+	return true;
+end
+
+function shallowcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in pairs(orig) do
+            copy[orig_key] = orig_value
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
